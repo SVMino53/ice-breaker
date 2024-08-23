0fad94a073e994b0362f0ffcce2f6ba92bd37f65
Isak Forsberg <46923173+SVMino53@users.noreply.github.com>
2024-08-23 11:26:09 +0200

    Update pxt.json, main.blocks, main.ts, main.py, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "E0BA-wz?-/2lXv[=DA#m":
+            case "Test":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 3 3 3 3 3 . . . . . . . 
+. . 3 3 3 3 3 3 3 3 3 3 . . . . 
+. . 3 3 3 3 3 3 3 3 3 3 3 . . . 
+. 3 3 3 3 3 3 f 3 3 3 3 3 3 . . 
+3 3 3 f f 3 3 3 f 3 3 3 3 3 . . 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+3 3 3 3 3 3 3 f 3 3 3 3 3 3 . . 
+3 3 3 3 f f f f 3 3 3 3 3 . . . 
+. 3 3 3 3 3 3 3 3 3 3 3 . . . . 
+. . 3 3 3 3 3 3 3 3 3 . . . . . 
+. . . . 3 3 3 3 3 3 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
